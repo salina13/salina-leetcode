e62b872c4438de4a0cfd84ea92b07edbabd00026
salina <salinamalla2001@gmail.com>
2024-04-09 09:33:50 +0545

    problem3

---
+++ problem3/solution3.js
@@ -0,0 +1,16 @@
+function createCounter(init){
+    let currentValue = init;
+
+    return {
+        increment: function() {
+            return ++currentValue;
+        },
+        decrement: function() {
+            return --currentValue;
+        },
+        reset: function() {
+            currentValue = init;
+            return currentValue;
+        }
+    };
+ }
