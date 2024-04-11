d5035e61ece1acf3e8044d3fac7a504e9e472229
salina <salinamalla2001@gmail.com>
2024-04-11 09:36:24 +0545

    baisc leetcode problems

---
+++ problem10/solution10.js
@@ -0,0 +1,15 @@
+var once = function(fn) {
+    let called = false;
+    let result;
+   
+   return function(...args){
+        if (!called) {
+     called = true;
+     result = fn(...args);
+     return result;
+    } else {
+     return undefined;
+        }    
+    };
+       
+}
